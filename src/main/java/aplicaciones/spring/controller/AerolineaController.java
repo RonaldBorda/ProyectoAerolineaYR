7ee0dd7dc095c3aa58a58886faa6eaff45d16cb7
RonaldBorda <ronald.borda@tecsup.edu.pe>
2021-11-30 12:36:20 -0500

    Implementacion CRUD de la entidad aeroline

--- src/main/java/aplicaciones/spring/controller/AerolineaController.java
+++ src/main/java/aplicaciones/spring/controller/AerolineaController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import aplicaciones.spring.models.entity.Aerolinea;
@@ -28,7 +31,24 @@ public class AerolineaController {
 		Aerolinea aerolinea = new Aerolinea();
 		model.addAttribute("titulo", "Registrar nueva Aerolinea");
 		model.addAttribute("aerolinea", aerolinea);
-		
 		return "formAerolinea";
 	}
+	@PostMapping("/guardar")
+	public String guardar(@ModelAttribute Aerolinea aerolinea) {
+		aerolineaService.guardar(aerolinea);
+		return "redirect:/aerolineas/listar";
+	}
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable("id") Long idAerolinea, Model model) {
+		Aerolinea aerolinea = aerolineaService.buscarPorId(idAerolinea);
+		model.addAttribute("titulo", "Editar Aerolinea");
+		model.addAttribute("aerolinea", aerolinea);
+		return "formAerolinea";
+	}
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable("id") Long idAerolinea) {
+		aerolineaService.eliminar(idAerolinea);
+		return "redirect:/aerolineas/listar";
+	}
+	
 }
