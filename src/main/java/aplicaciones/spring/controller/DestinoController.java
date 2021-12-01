9c7a26e6426776c3048a395ed2602e20b5d96a2c
RonaldBorda <ronald.borda@tecsup.edu.pe>
2021-11-30 22:07:56 -0500

    Implementacion CRUD a la entidad Destinos

--- src/main/java/aplicaciones/spring/controller/DestinoController.java
+++ src/main/java/aplicaciones/spring/controller/DestinoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import aplicaciones.spring.models.entity.Destino;
@@ -34,6 +35,18 @@ public class DestinoController {
 	@PostMapping("/guardar")
 	public String guardar(@ModelAttribute Destino destino) {
 		destinoService.guardar(destino);
+		return "redirect:/destinos/listar";
+	}
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable("id") Long idDestino, Model model) {
+		Destino destino = destinoService.buscarPorId(idDestino);
+		model.addAttribute("titulo", "Editar Destino");
+		model.addAttribute("destino", destino);
+		return "formDestino";
+	}
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable("id") Long idDestino) {
+		destinoService.eliminar(idDestino);
 		return "redirect:/destinos/listar";
 	}
 }
